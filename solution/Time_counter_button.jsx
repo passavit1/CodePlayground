77e510ab6dcc25fbefb9624644d26094565b0319
Wizat <passavit1@gmail.com>
2022-11-21 20:23:11 +0700

    commit

---
+++ solution/Time_counter_button.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [switched, setSwitched] = useState(false);
+  const AssignInterval = useRef("");
+
+  function Switch() {
+    setSwitched(switched === false ? true : false);
+  }
+
+  useEffect(() => {
+    if (switched) {
+      const powerOn = setInterval(() => {
+        setCount((prev) => prev + 1);
+      }, 1000);
+      AssignInterval.current = powerOn;
+    } else {
+      clearInterval(AssignInterval.current);
+    }
+  }, [switched]);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={Switch}>start</button>
+      <button onClick={Switch}>stop</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+  );
+}
+
+export default App;
