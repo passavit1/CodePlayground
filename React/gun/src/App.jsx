c553e22abb59ccb0a663b9f0cd0a9c59214161b8
Wizat <passavit1@gmail.com>
2022-12-03 18:05:01 +0700

    commit

---
+++ React/gun/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+function App() {
+  const arr = [
+    "what",
+    "is",
+    "your",
+    "name",
+    "my",
+    "name",
+    "is",
+    "passavit",
+    "maicharoen",
+  ];
+
+  const [count, setCount] = useState(0);
+  const [switched, setSwitched] = useState(false);
+  const AssignInterval = useRef("");
+
+  function Switch() {
+    setSwitched(switched === false ? true : false);
+  }
+
+  useEffect(() => {
+    if (switched) {
+      const powerOn = setInterval(() => {
+        setCount((prev) => prev + 1);
+      }, 1000);
+      AssignInterval.current = powerOn;
+    } else {
+      clearInterval(AssignInterval.current);
+    }
+  }, [switched]);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={Switch}>start</button>
+      <button onClick={Switch}>stop</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+  );
+}
+
+export default App;
