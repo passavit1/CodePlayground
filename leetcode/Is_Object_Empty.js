ce7b8adf7739d8c390b1ad7bfe6f4b9390995a19
Wizat <passavit1@gmail.com>
2023-06-28 17:52:11 +0700

    finish 2 solutions from leet code

---
+++ leetcode/Is_Object_Empty.js
@@ -0,0 +1,22 @@
+/*
+Given an object or an array, return if it is empty.
+
+An empty object contains no key-value pairs.
+An empty array contains no elements.
+You may assume the object or array is the output of JSON.parse.
+
+https://leetcode.com/problems/is-object-empty/
+
+*/
+
+var isEmpty = function (obj) {
+  return Object.keys(obj).length === 0; //การดึงเฉพาะ Key ออกมา หรือสามารถดึง Values ได้ดังนี้ Object.Values(obj)
+};
+
+let case1 = { x: 5, y: 42 };
+let case2 = {};
+let case3 = [null, false, 0];
+
+console.log("case1 result", isEmpty(case1));
+console.log("case2 result", isEmpty(case2));
+console.log("case3 result", isEmpty(case3));
