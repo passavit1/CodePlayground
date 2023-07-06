8cce902191f358f142dfcbb0c7f4f339e3ada7c4
Passavit_Gunn <passavit1@gmail.com>
2023-07-06 20:34:24 +0700

    practice leecode

---
+++ leetcode/practice.js
@@ -0,0 +1,13 @@
+let result = [];
+
+const two_sum = (number, target) => {
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      if (number[i] + number[j] === target) {
+        result.push([i, j]);
+      }
+    }
+  }
+
+  console.log(result);
+};
