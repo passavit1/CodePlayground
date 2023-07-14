046a909185d1f5716750fdffeb258069fc43bf9b
Wizat <passavit1@gmail.com>
2023-07-14 20:48:40 +0700

    practice leetcode

--- leetcode/practice.js
+++ leetcode/practice.js
@@ -1,5 +1,3 @@
-var createHelloWorld = function () {
-  return function (...args) {
-    return "Hello World";
-  };
+var isEmpty = function (obj) {
+  return Object.keys(obj).length === 0; //การดึงเฉพาะ Key ออกมา หรือสามารถดึง Values ได้ดังนี้ Object.Values(obj)
 };
