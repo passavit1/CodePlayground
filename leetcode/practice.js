a499207ba1f2a9ec5a1dc318f1de9a34dd765c38
Wizat <passavit1@gmail.com>
2023-07-24 23:39:39 +0700

    practice leetcode

--- leetcode/practice.js
+++ leetcode/practice.js
@@ -14,3 +14,40 @@ var createCounter = function (init) {
     },
   };
 };
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by 0 is not allowed");
+    }
+    this.result /= value;
+    return this;
+  }
+
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
